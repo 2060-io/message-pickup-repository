69bfbce6bff37c7cf288b3b6e6e000574682ac08
Gabriel Mata <gabrielmatau@gmail.com>
2025-04-09 17:03:32 -0500

    fix: FIx a name column encrypted_message in takeFromQueue function
    Signed-off-by: Gabriel Mata <gabrielmatau@gmail.com>

--- packages/postgres/src/PostgresMessagePickupRepository.ts
+++ packages/postgres/src/PostgresMessagePickupRepository.ts
@@ -187,7 +187,7 @@ export class PostgresMessagePickupRepository implements MessagePickupRepository 
         ORDER BY created_at 
         LIMIT $3
       )
-      RETURNING id, encryptedmessage, state;
+      RETURNING id, encrypted_message, state;
     `
       const params = [connectionId, recipientDid, limit ?? 0]
       const result = await this.messagesCollection?.query(query, params)
